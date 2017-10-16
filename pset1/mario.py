f182c676a3eff4abea71a88aa4b271173b3f8926
Alexander Troup <alextroup@outlook.com>
2017-10-16 11:20:43 +0000

    Cleaner code updates

--- pset1/mario.py
+++ pset1/mario.py
@@ -1,11 +1,18 @@
-height = None
+def pyramid_printer(height):
+    """Simply prints out a mario end stage pyramid"""
 
-while height < 0 or height > 23 or height == None:
-    try:
-        height = int(input("Height: "))
-    except ValueError:
-        continue
-    
-for i in range(1, height + 1):
-    string = "{}{}  {}".format(" " * (height - i),"#" * i, "#" * i)
-    print(string)
+    if height < 0 or height > 23:
+        return False
+
+    for i in range(1, height + 1):
+        string = "{}{}  {}".format(
+                                    " " * (height - i),
+                                    "#" * i,
+                                    "#" * i)
+        print(string)
+    return True
+
+
+if __name__ == "__main__":
+    pyramid_printer(15)
+
--- pset1/water.c
+++ pset1/water.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
+#define BOTTLE_SIZE_OZ 12
+
+/* very simple function that converts standard minute
+** of water use in a shower to bottles of water (12oz bottles)
+*/
 int main(void)
 {
-    // converts standard minute of water use in a shower to bottles of water (12oz bottles)
     printf("Minutes: ");
-    
+
     int minutes = get_int();
-    
-    printf("Bottles: %i\n", minutes * 12);
+
+    printf("Bottles: %i\n", minutes * BOTTLE_SIZE_OZ);
 }
