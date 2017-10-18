0a05c113b9cfeef548bfef57fc1afe887a3f0362
Alexander Troup <alextroup@outlook.com>
2017-10-18 06:50:20 +0000

    Update comment / style.

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,42 +1,46 @@
 #include <cs50.h>
 #include <stdio.h>
 
+#define SUCCESS 0 /* return value for success */
 
+/* prints out a mario 'pyramid' as at the end of levels */
 int main(void)
 {
-    int height;
-    
-    // wait until appropriate height is entered
-    do
-    {
+    int height = 0;
+
+    int i = 0;
+    int j = 0;
+
+    /* wait until appropriate height is entered */
+    do {
         printf("Height: ");
         height = get_int();
     }
     while (height < 0 || height > 23);
-    
-    // iterate for each line starting 1
-    for (int i = 1; i <= height; i++)
-    {
-        // draw spaces
-        for (int j = 1; j <= height - i; j++) 
-        {
+
+    /* iterate for each line starting 1 */
+    for (i = 1; i <= height; i++) {
+
+        /* draw spaces */
+        for (j = 1; j <= height - i; j++) {
             printf(" ");
         }
-        
-        // draw hashes
-        for (int j = 1; j <= i; j++) 
-        {
-            printf("#"); 
+
+        /* draw hashes */
+        for (j = 1; j <= i; j++) {
+            printf("#");
         }
-        // draw space between both sides of the 'pyramid'    
+
+        /* draw space between both sides of the 'pyramid' */
         printf("  ");
-        
-        // draw second side of pyramid (just hashes, and then new line char)
-        for (int j = 1; j <= i; j++)
-        {
-            printf("#"); 
+
+        /* draw second side of pyramid */
+        for (j = 1; j <= i; j++) {
+            printf("#");
         }
-        
+
         printf("\n");
     }
+
+    return SUCCESS;
 }
