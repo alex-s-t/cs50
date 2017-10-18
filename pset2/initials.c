21e0080c08263e4012a43edd9356c5819be7fc45
Alexander Troup <alextroup@outlook.com>
2017-10-18 02:09:08 +0000

    Fixed out of bounds array access.

--- pset2/initials.c
+++ pset2/initials.c
@@ -3,23 +3,34 @@
 #include <string.h>
 #include <ctype.h>
 
+#define INITIAL_CHAR 0 /* first char in input string */
+#define SUCCESS 0      /* return value for success */
+
+/* prints out the initials of a name
+** e.g. alex apple -> AA */
 int main(void)
 {
     char *name = get_string();
-    
-    // checks first char and prints if not a space
-    if (name[0] != ' ') {
-        printf("%c", toupper(name[0]));
+    int i = 0;
+
+    int name_length = strlen(name);
+
+    /* checks first char and prints if not a space */
+    if (name[INITIAL_CHAR] != ' ') {
+        printf("%c", toupper(name[INITIAL_CHAR]));
     }
-    
-    // iterate through all characters in 'name'
-    for(int i = 0; i < strlen(name); i++)
-    {
-        // prints char that immediately follows a space, but that is not a space itself
+
+    /* iterate through all characters in 'name' */
+    for(i = 1; i < name_length; i++) {
+
+        /* prints char that immediately follows a space,
+        ** but that is not a space itself */
         if (name[i - 1] == ' ' && name[i] != ' ') {
             printf("%c", toupper(name[i]));
         }
     }
 
     printf("\n");
+
+    return SUCCESS;
 }
