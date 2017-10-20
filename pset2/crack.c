eba6459708ce6c515aeeed423604a0e0eb020e0e
Alexander Troup <alextroup@outlook.com>
2017-10-20 12:19:32 +0000

    Style comments update

--- pset2/crack.c
+++ pset2/crack.c
@@ -5,7 +5,17 @@
 #include <stdlib.h>
 
 
+/* constants */
 #define MAX_PW_LENGTH 5
+#define SALT_LENGTH 3
+
+#define MIN_CMD_ARGUMENTS 2
+
+#define SUCCESS 0
+#define FAILURE 1
+
+#define PW_FOUND 2
+#define PW_NOT_FOUND 0
 
 
 /* function prototype */
@@ -16,43 +26,50 @@ int string_comparer(char *q, char *s, char *salt);
 int main(int argc, char *argv[])
 {
 
-    if (argc != 2) {
+    if (argc != MIN_CMD_ARGUMENTS) {
         printf("Usage: ./crack hash\n");
-        return 1;
+        return FAILURE;
     }
 
     char *s = argv[1];
-    char salt[3] = {'5', '0', '\0'};
-    char test_string [5] = { '\0' }; test_string[0] = 'a';
-    int length;
+    char salt[SALT_LENGTH] = {'5', '0', '\0'};
+    char test_string [MAX_PW_LENGTH] = { '\0' }; test_string[0] = 'a';
+
+    int length = 0;
+    int i = 0;
+    int j = 0;
 
     while (strlen(test_string) < MAX_PW_LENGTH) {
 
+
         length = strlen(test_string);
-        // check to see if max combinations achieved (all 'Z' in array for given length)
-        for (int i = 0; i < length; i++) {
+
+        /* check to see if max combinations achieved (all 'Z' in array) */
+        for (i = 0; i < length; i++) {
             if (test_string[i] != 'Z') {
                 break;
             }
-            for (int j = 0; j < length + 1; j++) {
+            for (j = 0; j < length + 1; j++) {
                 test_string[j] = 'a';
             }
         }
 
-        // iterate through word
-        for (int i = length - 1; i > -1; i--) {
+        /* iterate through word */
+        for (i = length - 1; i > -1; i--) {
             /* if this condition is true, but the ones following it are not,
             it's the only case in which the for loop will continue */
             if (test_string[i] == 'Z') {
             test_string[i] = 'a';
 
-                // check if preceding letter needs to be changed (any letter preceding a Z that isn't itself Z)
+                /* check if preceding letter needs to be changed
+                ** (any letter preceding a Z that isn't itself Z) */
                 if (test_string[i - 1] != 'Z') {
                     if (test_string[i - 1] == 'z') {
                         test_string[i - 1] = 'A';
                         break;
                     }
-                    // if it's not a Z, safe to change i - 1. Otherwise we'll iterate around again.
+                    /* if it's not a Z, safe to change i - 1.
+                    ** Otherwise we'll iterate around again. */
                     else {
                         test_string[i - 1] = test_string[i - 1] + 1;
                         break;
@@ -60,7 +77,8 @@ int main(int argc, char *argv[])
                 }
             }
 
-            // this will only trigger if the first letter checked isn't an uppercase Z.
+            /* this will only trigger if the first
+            ** letter checked isn't an uppercase Z. */
             else {
                 if (test_string[i] == 'z') {
                     test_string[i] = 'A';
@@ -73,26 +91,29 @@ int main(int argc, char *argv[])
             }
         }
 
-    if (string_comparer(test_string, s, salt)) {
-        printf("%s\n", test_string);
-        return 0;
-    }
+        /* test the target string */
+        if (string_comparer(test_string, s, salt)) {
+            printf("%s\n", test_string);
+            return SUCCESS;
+        }
 
 
     }
-    // end while loop
-
+    /* end while loop */
 }
 
-// function for comparing input string to encrypted's string output
+
+/* compares input string to encrypted's string output */
 int string_comparer(char *q, char *s, char *salt)
 {
+
+    int i = 0;
     char *string_enc = crypt(q, salt);
 
-    for (int i = 0; i < strlen(s); i++) {
+    for (i = 0; i < strlen(s); i++) {
         if (s[i] != string_enc[i]) {
-            return 0;
+            return PW_NOT_FOUND;
         }
     }
-    return 1;
+    return PW_FOUND;
 }
