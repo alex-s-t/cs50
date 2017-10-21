7e3c0bb2b43d6193818e69651d12119142e5c7a0
Alexander Troup <alextroup@outlook.com>
2017-10-21 09:55:43 +0000

    isalpha / style updates

--- pset2/devigenere.c
+++ pset2/devigenere.c
@@ -3,42 +3,45 @@
 #include <stdio.h>
 #include <ctype.h>
 
+
+/* decrypts the vigenere cipher */
 int main(void)
 {
-    // gets encryption key
+    /* gets encryption key */
     char *s_key = get_string();
-    // gets encrypted text
+    /* gets plain text */
     char *s_text = get_string();
+    /* counter variable for encryption */
     int k = 0;
-    
-    for (int i = 0; i < strlen(s_text); i++)
-    {
-        // loops through key
-        int j = (i - k) % strlen(s_key);
-        
-        // print non-alhabetical characters (spaces etc.)
-        if ((s_text[i] > 90 && s_text[i] < 97) || s_text[i] < 65 || s_text[i] > 122) {
+    int i = 0;
+    int j = 0;
+
+    /* iterate through plaintext chars */
+    for (i = 0; i < strlen(s_text); i++) {
+        /* loops through key */
+        j = (i - k) % strlen(s_key);
+
+        /* print non-alhabetical characters (spaces etc.) */
+        if (!isalpha(s_text[i])) {
             printf("%c", s_text[i]);
             k += 1;
         }
-        
-        // uppercase chars
+
+        /* uppercase chars */
         else if (s_text[i] >= 65 && s_text[i] <= 90) {
             int m = s_text[i] - 65;
             int g = (toupper(s_key[j])) - 65;
-            // modulo in C, due to negative numbers
-            int z = (((m - g) % 26) + 26) % 26; 
+            /* modulo in C, due negative number modulo rounding */
+            int z = (((m - g) % 26) + 26) % 26;
             printf("%c", z + 65);
         }
-        
-        // lowercase chars
+
+        /* lowercase chars */
         else if (s_text[i] >= 97 && s_text[i] <= 122) {
             int m = s_text[i] - 97;
             int g = (toupper(s_key[j])) - 65;
             int z = (((m - g) % 26) + 26) % 26;
             printf("%c", z + 97);
-
         }
     }
-    
 }
