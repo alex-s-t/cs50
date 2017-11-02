3aefe90734aace1eb25bc611031df302f1a9e36c
Alexander Troup <alextroup@outlook.com>
2017-11-02 03:25:18 +0000

    Initialise name char array with non garbage values.

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -24,37 +24,37 @@ int main(int argc, char *argv[])
         return FAILURE;
     }
 
-    // open the card file
+    /* open the card file */
     FILE *inptr = fopen(argv[1], "r");
     if (inptr == NULL) {
         fprintf(stderr, "Could not open %s\n", argv[1]);
         return OPEN_FILE_ERROR;
     }
 
-    // buffer array to store blocks
+    /* buffer array to store blocks */
     BYTE buffer[JPEG_BLOCK];
-    // namer/tracker
+    /* namer/tracker */
     int jpg_count = 0;
-    char name[MAX_RECOVERY_NAME];
-    // declare file pointer
+    char name[MAX_RECOVERY_NAME] = { '\0' };
+    /* declare file pointer */
     FILE *outptr = NULL;
 
-    // commence iteration through card image
+    /* commence iteration through card image */
     while ((fread(&buffer, JPEG_BLOCK, 1, inptr) == 1) && jpg_count < MAX_RECOVERY) {
-    // read file (struct, size of element, number of elements to read, file pointer)
+    /* read file (struct, size of element, number of elements to read, file pointer) */
 
-    // if it's a jpeg header...
+    /* if it's a jpeg header... */
     if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)) {
-        // close previous jpeg file if open
+        /* close previous jpeg file if open */
         if (outptr != NULL) {
             fclose(outptr);
         }
-        // create new filename
+        /* create new filename */
         sprintf(name, "%03i.jpg", jpg_count);
 
         jpg_count += 1;
 
-        // open a new jpeg file
+        /* open a new jpeg file */
         outptr = fopen(name, "w");
         if (outptr == NULL) {
             fprintf(stderr, "Could not open new outfile %s\n", name);
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
     }
 
     if (outptr != NULL) {
-        // write the bytes of the jpeg
+        /* write the bytes of the jpeg */
         fwrite(buffer, JPEG_BLOCK, 1, outptr);
         }
     }
